955d1993371d5bc9ef77d50f0e5a2e1fd73bf2ae
Ian Agpawa <agpawaji@gmail.com>
2016-09-12 03:24:43 +0000

    finished function

--- public/javascripts/conversion.js
+++ public/javascripts/conversion.js
@@ -1,26 +1,45 @@
+
+
 function checkInput(input){
-  
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   
-  function convertToNatural(number){
-    var date = new Date(number * 1000)
+    function dateIt(date){
     var year = date.getFullYear()
     var month = months[date.getMonth()] 
     var day = date.getDate()
-    var natural = month + " " + day + ", " + year
-    return natural
+    return month + " " + day + ", " + year
   }
   
+  
+  function convertToNatural(uInput){
+    var date = new Date(uInput * 1000)
+    return dateIt(date)
+  }
+  
+
   function convertToUnix(string){
     var date = new Date(string)
-    return date
+    return date.getTime()
   }
   
-  if (!isNaN(input)){
-    var naturalDate = convertToNatural(input)
-    return ({ unix: input, natural: naturalDate })
+  
+  
+  var naturalDate;
+  var unixDate;
+  if (!isNaN(input)){ // input is unix
+    naturalDate = convertToNatural(input)
+    unixDate = input
   } else {
-    return convertToUnix(input)
-  }               
+    var date = new Date(input)
+    if (date != 'Invalid Date'){ // input is valid string
+      unixDate = convertToUnix(input)
+      naturalDate = dateIt(date)
+    } else { //  input is invalid string
+      console.log({ unix: null, natural: null })
+    }
+  }
+   console.log({unix: unixDate, natural: naturalDate})
 }
 
+
+checkInput('02/28/98')
--- routes/index.js
+++ routes/index.js
@@ -15,37 +15,53 @@ exports.index = function(req, res){
 
 
 
+
 function checkInput(input){
-  
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   
-  function convertToNatural(number){
-    var date = new Date(number * 1000)
+    function dateIt(date){
     var year = date.getFullYear()
     var month = months[date.getMonth()] 
     var day = date.getDate()
-    var natural = month + " " + day + ", " + year
-    return natural
+    return month + " " + day + ", " + year
   }
   
+  
+  function convertToNatural(uInput){
+    var date = new Date(uInput * 1000)
+    return dateIt(date)
+  }
+  
+
   function convertToUnix(string){
     var date = new Date(string)
-    return date
+    return date.getTime()
   }
   
-  if (!isNaN(input)){
-    var naturalDate = convertToNatural(input)
-    return ({ unix: input, natural: naturalDate })
+  
+  
+  var naturalDate;
+  var unixDate;
+  if (!isNaN(input)){ // input is unix
+    naturalDate = convertToNatural(input)
+    unixDate = input
   } else {
-    return convertToUnix(input)
-  }            
+    var date = new Date(input)
+    if (date != 'Invalid Date'){ // input is valid string
+      unixDate = convertToUnix(input)
+      naturalDate = dateIt(date)
+    } else { //  input is invalid string
+      return ({ unix: null, natural: null })
+    }
+  }
+   return ({unix: unixDate, natural: naturalDate})
 }
+
 
 
 
 
 exports.getTime = function(req, res){
   var userInput = req.params.input
-  var output = checkInput(userInput)
-  res.send(output)
+  res.json(checkInput(userInput))
 }
