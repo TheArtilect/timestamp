be5b22c2ec0e9704c121353fdd69dce65316d087
Ian Agpawa <agpawaji@gmail.com>
2016-09-19 23:10:39 -0400

    added descrip to home

--- routes/index.js
+++ routes/index.js
@@ -19,30 +19,30 @@ exports.index = function(req, res){
 function checkInput(input){
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  
+
   function dateIt(date){
     var year = date.getFullYear()
-    var month = months[date.getMonth()] 
+    var month = months[date.getMonth()]
     var day = date.getDate()
     var namedDate = days[date.getDay()]
-    return [(month + " " + day + ", " + year), namedDate] 
+    return [(month + " " + day + ", " + year), namedDate]
   }
-  
-  
+
+
   function convertToNatural(uInput){
     var date = new Date(uInput * 1000)
     return dateIt(date)
   }
-  
+
 
   function convertToUnix(string){
     var date = new Date(string)
     return date.getTime() / 1000
   }
-  
-  
-  
-  
+
+
+
+
   var naturalDate;
   var unixDate;
   var day;
@@ -62,7 +62,7 @@ function checkInput(input){
       arr = dateIt(date)
       naturalDate = arr[0]
       day = arr[1]
-      
+
     }
   }
    return ({unix: unixDate, natural: naturalDate, dayOfTheWeek: day})
@@ -75,5 +75,5 @@ function checkInput(input){
 exports.getTime = function(req, res){
   var userInput = req.params.input
   res.json(checkInput(userInput))
-  
+
 }
