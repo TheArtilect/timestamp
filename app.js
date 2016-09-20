0c20af2884cc48d9964060785d80866b9c5ae2e7
Ian Agpawa <agpawaji@gmail.com>
2016-09-19 21:05:54 -0400

    minor changes to front page

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,4 +40,3 @@ var server = http.createServer(app)
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
